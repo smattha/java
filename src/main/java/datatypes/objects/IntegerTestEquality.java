ba03b46c818736737abfac37357e3ed0234fa1ed
Matthaiakis Stergios <stergiosmat@gmail.com>
2020-01-05 10:03:34 +0200

    --Integer wrapper

---
+++ src/main/java/datatypes/objects/IntegerTestEquality.java
@@ -0,0 +1,22 @@
+package  datatypes.objects;;
+
+/***
+ * @author MAtthaiakis Stergios
+ */
+
+public class IntegerTestEquality {
+
+    public static void main(String[] args) {
+
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Integer wrapper simple test");
+        // instantiate Integer objects
+        Integer firstInteger = 100;
+        Integer secondInteger = new Integer(100);
+
+        // test for equality
+        System.out.println(firstInteger.equals(secondInteger));
+
+    }
+
+}
