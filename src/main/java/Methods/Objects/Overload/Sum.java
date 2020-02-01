eda42acc7b34765e74984be34dca1ace83e59d6c
Matthaiakis Stergios <stergiosmat@gmail.com>
2020-02-01 03:04:13 +0200

    --Overloads method in java, sum example

---
+++ src/main/java/Methods/Objects/Overload/Sum.java
@@ -0,0 +1,43 @@
+package Methods.Objects.Overload;
+
+/**
+ * @author Matthaiakis Stergios
+ */
+
+public class Sum {
+
+    // Overloaded sum(). This sum takes two int parameters
+    public int sum(int x, int y)
+    {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Sum int, int ");
+        return (x + y);
+    }
+
+    // Overloaded sum(). This sum takes three int parameters
+    public int sum(int x, int y, int z)
+    {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Sum int int int ");
+        return (x + y + z);
+    }
+
+    // Overloaded sum(). This sum takes two double parameters
+    public double sum(double x, double y)
+    {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Sum double double");
+        return (x + y);
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Overlad method");
+        Sum s = new Sum();
+        System.out.println(s.sum(10, 20));
+        System.out.println(s.sum(10, 20, 30));
+        System.out.println(s.sum(10.5, 20.5));
+    }
+}
