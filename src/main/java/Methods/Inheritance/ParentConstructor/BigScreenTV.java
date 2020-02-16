acbde4a776ffa23dbdd4d73e298198b72ae7cab3
Matthaiakis Stergios <stergiosmat@gmail.com>
2020-02-16 04:24:17 +0200

    --Inheritance and constructors add simple example

---
+++ src/main/java/Methods/Inheritance/ParentConstructor/BigScreenTV.java
@@ -0,0 +1,43 @@
+
+package Methods.Inheritance.ParentConstructor;
+
+/**
+ * @author Matthaiakis Stergios
+ */
+class BigScreenTV extends Television
+{
+	private String aspectRatio;
+	private short size;
+
+	BigScreenTV(int channel)
+	{
+		this("unknown", (short) 40, channel);
+		System.out.println("--------------------------------------------------------");
+		System.out.println("Inside BigScreenTV(int)");
+		System.out.println("--------------------------------------------------------");
+	}
+
+	BigScreenTV(String r, short s, int channel)
+	{
+		super(channel);
+
+		System.out.println("--------------------------------------------------------");
+		System.out.println("Inside BigScreenTV(String, short, int)");
+		System.out.println("--------------------------------------------------------");
+
+		aspectRatio = r;
+		size = s;
+		
+	}
+
+	BigScreenTV(String r, short s, int channel, int volume)
+	{
+		super(channel, volume);
+
+		System.out.println("--------------------------------------------------------");
+		System.out.println("Inside BigScreenTV(String, short, int, int)");
+		System.out.println("--------------------------------------------------------");
+		aspectRatio = r;
+		size = s;
+	}
+}
---
+++ src/main/java/Methods/Inheritance/ParentConstructor/ParentDemo.java
@@ -0,0 +1,28 @@
+package Methods.Inheritance.ParentConstructor;
+
+/**
+ * @author Matthaiakis Stergios
+ */
+
+public class ParentDemo
+{
+	public static void main(String [] args)
+	{
+		System.out.println("*** big screen #1 ***");
+		int channel = 4;
+		new BigScreenTV(channel);
+
+		short size = 53;
+		channel = 3;
+		String ratio = "16:9";
+		System.out.println("\n*** big screen #2 ***");
+		new BigScreenTV(ratio, size, channel);
+
+		ratio = "5:4";
+		size = 42;
+		channel = 4;
+		int volume = 7;
+		System.out.println("\n*** big screen #3 ***");
+		new BigScreenTV(ratio, size, channel, volume);
+	}
+}
