c4e4fc6881e5a5487855819a9d3439707dc81610
Matthaiakis Stergios <stergiosmat@gmail.com>
2020-03-19 22:02:00 +0200

    --Collections demos

---
+++ src/main/java/Collections/DoubleArray.java
@@ -0,0 +1,34 @@
+package Collections;
+
+/**
+ * @author Matthaiakis Stergios
+ */
+
+public class DoubleArray
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Instantiating a double array");
+		int [] [] sums = new int[10][12];
+
+		System.out.println("Filling the array with data");
+		for(int row = 0; row < 10; row++)
+		{
+			for(int col = 0; col < 12; col++)
+			{
+				sums[row][col] = row + col;
+			}
+		}
+
+		System.out.println("Displaying the array");
+		for(int row = 0; row < sums.length; row++)
+		{
+			for(int col = 0; col < sums[row].length; col++)
+			{
+				System.out.print(sums[row][col] + "\t");
+			}
+			System.out.println();
+		}
+
+	}
+}
