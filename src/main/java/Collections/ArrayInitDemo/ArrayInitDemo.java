00409f07aa3640b35664679fd6bcf0b1b91722b4
Matthaiakis Stergios <stergiosmat@gmail.com>
2020-03-23 18:54:20 +0200

    --ArrayDemo

---
+++ src/main/java/Collections/ArrayInitDemo/ArrayInitDemo.java
@@ -0,0 +1,40 @@
+package Collections.ArrayInitDemo;
+
+/**
+ * @author Matthaiakis Stergios
+ */
+
+
+public class ArrayInitDemo
+{
+	public static void main(String [] args)
+	{
+		int [] odds = {1, 3, 5, 7, 9};
+		System.out.println("odds.length = " + odds.length);
+		for(int i = 0; i < odds.length; i++)
+		{
+			System.out.println("odds[" + i + "] = " + odds[i]);
+		}
+
+		String [] daysOfWeek = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+		System.out.println("\ndaysOfWeek.length = " + daysOfWeek.length);
+		for(int i = 0; i < daysOfWeek.length; i++)
+		{
+			System.out.println("daysOfWeek[" + i + "] = " + daysOfWeek[i]);
+		}
+
+
+		Employee [] employees = {new Employee("M. Mouse", "Main St. USA", 1),
+								new Employee("D. Duck", "Lake Buena Vista", 2),
+								new Employee("W. Pooh", "100 Acre St.", 3)};
+
+		System.out.println("\nemployees.length = " + employees.length);
+		for(int i = 0; i < employees.length; i++)
+		{
+			employees[i].mailCheck();
+		}
+
+
+
+	}
+}
